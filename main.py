34ea281331e99dece702138f2afa4c0ece9f258c
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-11-01 18:38:40 -0400

    Implement edge detection algorithm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import cv2
 from pylibCZIrw import czi
-import sys
 from os import walk, path
+import numpy as np
+import cv2
+import sys
 
 
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
@@ -16,5 +17,24 @@ filenames = filenames[2]
 total_area = 0
 for filename in filenames:
     with czi.open_czi(path.join(dirname, filename)) as czi_file:
-        frame = czi_file.read()
-        print(frame)
+        # Read image
+        frame = czi_file.read(pixel_type="Bgr48")
+
+        # Prepare image for edge detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=0.5)
+        blurred = cv2.convertScaleAbs(blurred)
+
+        # Calculate thresholds for Canny
+        v = np.median(blurred)
+        lower = int(max(0, (1.0 - 0.33) * v))
+        upper = int(min(255, (1.0 + 0.33) * v))
+
+        edges = cv2.Canny(blurred, lower, upper)
+
+        # Display the image
+        cv2.imshow("Edges", edges)
+        while True:
+            # Exit the loop when 'q' key is pressed
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
