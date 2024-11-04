7606d9c1837f067435d221bfcaf870bba4138c58
Maxine Reyes Gomez <rg.maxine100@gmail.com>
2024-11-04 13:42:27 -0500

    Implement better thresholding and remove canny edge detection

--- main.py
+++ main.py
@@ -5,36 +5,66 @@ import cv2
 import sys
 
 
-if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print("Usage: python main.py <directory>")
-    exit()
+def main():
+    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print("Usage: python main.py <directory>")
+        exit()
 
-# get all files in directory
-filenames = next(walk(sys.argv[1]), (None, None, []))
-dirname = filenames[0] if filenames[0] else sys.argv[1]
-filenames = filenames[2]
+    # get all files in directory
+    filenames = next(walk(sys.argv[1]), (None, None, []))
+    dirname = filenames[0] if filenames[0] else sys.argv[1]
+    filenames = filenames[2]
 
-total_area = 0
-for filename in filenames:
-    with czi.open_czi(path.join(dirname, filename)) as czi_file:
-        # Read image
-        frame = czi_file.read(pixel_type="Bgr48")
+    for filename in filenames:
+        with czi.open_czi(path.join(dirname, filename)) as czi_file:
 
-        # Prepare image for edge detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blurred = cv2.GaussianBlur(src=gray, ksize=(3, 5), sigmaX=0.5)
-        blurred = cv2.convertScaleAbs(blurred)
+            # Read image
+            # Possible values are: Gray8, Gray16, Gray32Float, Bgr24, Bgr48, Bgr96Float
+            frame = czi_file.read(pixel_type="Bgr48").astype(np.uint8)
 
-        # Calculate thresholds for Canny
-        v = np.median(blurred)
-        lower = int(max(0, (1.0 - 0.33) * v))
-        upper = int(min(255, (1.0 + 0.33) * v))
+            # Prepare image for edge detection
+            show(frame)
+            grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(src=grey, ksize=(7, 7), sigmaX=0.5, borderType=cv2.BORDER_REFLECT)
+            lap = cv2.Laplacian(blur, cv2.CV_8UC1)
+            # sobel = cv2.Sobel(src=lap, ddepth=cv2.CV_32F, dx=1, dy=1, ksize=5)
+            show(lap)
+            blur = cv2.GaussianBlur(lap, (5, 5), 0)
+            ret,thresh = cv2.threshold(blur,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            # thresh = cv2.adaptiveThreshold(lap, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+            #                                cv2.THRESH_BINARY, 11, 7)
 
-        edges = cv2.Canny(blurred, lower, upper)
+            edges = thresh
+            # edges = cv2.Sobel(src=thresh, ddepth=cv2.CV_8UC1, dx=1, dy=1, ksize=5)
+            show(edges)
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+            bin_img = cv2.morphologyEx(thresh,
+                                       cv2.MORPH_CLOSE,
+                                       kernel,
+                                       iterations=3)
+            show(bin_img)
 
-        # Display the image
-        cv2.imshow("Edges", edges)
-        while True:
-            # Exit the loop when 'q' key is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # Find contours and draw them
+            cntrs, _heir = cv2.findContours(bin_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
+            # approx = cv2.approxPolyDP(cntrs[0], 0.1 * cv2.arcLength(cntrs[0], True), True)
+            edges = cv2.drawContours(bin_img, cntrs, -1, (0,255,0))
+            show(edges)
+
+
+def show(img):
+    cv2.imshow("", img)
+    while True:
+        # Exit the loop when 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def canny(img):
+    v = np.median(img)
+    lower = int(max(0, (1.0 - 0.33) * v))
+    upper = int(min(255, (1.0 + 0.33) * v))
+    return cv2.Canny(img, lower, upper)
+
+
+if __name__ == "__main__":
+    main()
